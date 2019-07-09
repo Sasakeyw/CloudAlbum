541682538404880b91a0bbff6dd646cc3e38b325
Sasakeyw <52242137+Sasakeyw@users.noreply.github.com>
2019-07-09 22:57:55 +0900

    mod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: 'AIzaSyDn9OLwdpnC8Vr7w_jPCvj-EFwDuouLBtk',
-    authDomain: 'cloudalbum-8053d.firebaseapp.com',
-    databaseURL: 'https://cloudalbum-8053d.firebaseio.com',
-    projectId: 'cloudalbum-8053d',
-    storageBucket: 'cloudalbum-8053d.appspot.com',
-    messagingSenderId: '244609537947',
-    appId: '1:244609537947:web:f56ef0f23b0d8d7f'
+
   }
 };
