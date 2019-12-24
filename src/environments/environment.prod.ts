c4d43ad35bc0aaf39cd86134ee391403c4d92939
Sasakeyw <52242137+Sasakeyw@users.noreply.github.com>
2019-12-24 16:02:46 +0900

    Secure
    Secure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,10 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: 'AIzaSyDaZtw_o1xYaFQZcUd-9Fs1YOmWTtq1wXY',
-    authDomain: 'cloudalbum20190927sasaki.firebaseapp.com',
-    databaseURL: 'https://cloudalbum20190927sasaki.firebaseio.com',
-    projectId: 'cloudalbum20190927sasaki',
-    storageBucket: 'cloudalbum20190927sasaki.appspot.com',
-    messagingSenderId: '686107530603',
-    appId: '1:686107530603:web:0afa13ea54872e4958682e',
-    measurementId: 'G-CRF0RG3VT2'
+
   },
   visionApi : {
-    apiKey : 'AIzaSyDDCUX3A8ZxBTomVsC88BLBE7ndM4lln7E'
+  
   }
 
 };
